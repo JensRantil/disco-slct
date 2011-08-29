3e6dac33d58ea8372f0448e9d6657e65225761cd
Jens Rantil <jens.rantil@gmail.com>
2011-08-30 01:03:13 +0200

    Revisited all job implementations.
    None of these jobs has been fully tested as of yet. dslct.py also needs to be revisited
    soon. That is part of the testing...

--- dslct_jobs.py
+++ dslct_jobs.py
@@ -37,44 +37,81 @@ class WordPruner(Job):
 	def reduce(iter, params):
 		for word, counts in kvgroup(sorted(iter)):
 			yield (word, 0), str(sum(counts))
-	
+
 	combiner = staticmethod(sum_combiner)
-	
+
 	# Output format should be DiscoDB for fast retrieval
 	reduce_output_stream = discodb_stream
 
 
 class WordToSentence(Job):
 	"""Generates (Word, 1) => Sentence"""
-	map_reader = staticmethod(chain_reader)
-	
 	@staticmethod
 	def map(sentence, threshold):
 		for word in line.split():
 			yield (word, 1), sentence
 
 
-class ClusterConstructor(Job):
-	"""Constructs the cluster elements.
-
-	It is crucial the input to this reduce is sorted! In other words, set
-	sort=True.
+class SentenceWordJoiner(Job):
+	"""Joins word counts and sentences.
+	Input: [(string:Word, 0) => int:WordCount | (string:Word, 1) => [string:Sentence]]
+	Output: [Sentence => [{Word: WordCount}]]
 	"""
+	map_reader = staticmethod(chain_reader)
 
 	@staticmethod
         def reduce(iter, params):
-		from itertools import groupby
-		
-		for (word, weight), data in groupby(iter, lambda kv: kv[0]):
+		for (word, weight), data in kvgroup(sorted(iter)):
 			if weight==0:
-				wordcounts = dict([(word, count) for count in data])
+				counts = data
+				wordcounts = {word: sum(counts)}
 				last_word = word
 			elif weight==1:
-				if word==last_word
-				
+				sentence = data
+				if word==last_word:
+					yield sentence, wordcounts
+
 
 	@staticmethod
 	def partition(key, npartitions):
 		word, _weight = key
 		return hash(word) % npartitions
+
+
+def combine(dict1, dict2):
+	"""Combine two wordcount dicts.
+
+	Example:
+	>>> combine({}, {'a':5})
+	{'a': 5}
+	>>> combine({'a': 5}, {'b': 6})
+	{'a': 5, 'b': 6}
+	>>> combine({'b': 6}, {'a': 5})
+	{'a': 5, 'b': 6}
+	>>> combine({'a': 5}, {'a': 6})
+	{'a': 11}
+	"""
+	from itertools import chain, groupby
+	newmap_items = groupby(sorted(chain(a.iteritems(), b.iteritems())), lambda item: item[0])
+	return dict([(word, sum(counts)) for word, counts in newmap_items])
+
+
+class ClusterConstructor(Job):
+	"""Constructs the clusters.
+	Input: [Sentence => [{Word: WordCount}]]
+	Output: [[Word | None] => 1]
+	"""
+	map_reader = staticmethod(chain_reader)
+
+	@staticmethod
+	def reduce(iter, params):
+		for sentence, wordcounts in kvgroup(sorted(iter)):
+			unionized_wordcounts = reduce(wordcounts, combine, {})
+			yield [word if unionized_wordcounts.has_key(word) else None for word in sentence.split()], 1
+
+
+class Summer(Job):
+	map_reader = staticmethod(chain_reader)
+	reduce = staticmethod(sum_reduce)
+	combiner = staticmethod(sum_combiner)
 
